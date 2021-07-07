7920e633b7b72db7a581d8e316317acb0f63a90c
Jakub Kákona <kaklik@mlab.cz>
2021-07-07 19:39:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'Natural Language :: Czech',
         # 'Operating System :: OS Independent',
         # 'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
     ]
 )
 
